1570db8c3c620013d0698c7ac792ed299ee585ce
nina-penot <nina.penot@laplateforme.io>
2026-07-21 14:41:32 +0200

    Implémentation du layout dans App
    - Implémentation de DefaultLayout dans App.jsx
    - Ajout dans Home.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import './App.css';
+import DefaultLayout from './layouts/DefaultLayout';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+      <Route element={<DefaultLayout />}>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Route>
+    </Routes>
   )
 }
 
