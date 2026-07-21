f6b1d5bd6a36d712bccbaf6b4a19301d5142bd1b
nina-penot <nina.penot@laplateforme.io>
2026-07-21 11:36:50 +0200

    En cours : écriture du layout par défaut

---
+++ FRONTEND/src/layouts/DefaultLayout.jsx
@@ -0,0 +1,9 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+
+export default function DefaultLayout() {
+    return (
+        <Header></Header>
+    )
+}
